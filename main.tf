30b60e40e4771e07f9115e66105cc7f76a0424e7
faangle <hr@faangpath.co.in>
2023-08-26 18:09:23 +0530

    coo

--- main.tf
+++ main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-  region  = "eu-north-1"
-}
-
 resource "aws_instance" "appl_server" {
   ami           = "ami-065681da47fb4e433"
   instance_type = "t2.micro"
