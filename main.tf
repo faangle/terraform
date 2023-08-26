cfd575ba3f041e9ef186e16812d12d2e192aefbd
faangle <hr@faangpath.co.in>
2023-08-26 18:04:50 +0530

    updated

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
   region  = "eu-north-1"
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_instance" "appl_server" {
   ami           = "ami-065681da47fb4e433"
   instance_type = "t2.micro"
 
