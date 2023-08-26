c95df2140d8b9064b28113976a53375041c70f75
faangle <hr@faangpath.co.in>
2023-08-26 17:51:33 +0530

    updated terraform main config

--- main.tf
+++ main.tf
@@ -1,4 +1,24 @@
-resource "aws_instance" "example" {
-  ami           = lookup(var.AMIS, var.AWS_REGION, "") # last parameter is the default value
-  instance_type = "t3.micro"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
 }
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
