52b64e338b179a5a74eabe14bc507aed2b85fdad
faangle <hr@faangpath.co.in>
2023-08-26 22:47:01 +0530

    modifed values

--- output.tf
+++ output.tf
@@ -5,3 +5,14 @@
 #output "instance_name" {
 #  value = aws_instance.example_instance.tags.Name
 #}
+
+output "instance_details" {
+  value = [
+    for i in aws_instance.example_instance :
+    {
+      id   = i.id
+      name = var.instance_names[count.index]
+      ip   = i.public_ip
+    }
+  ]
+}
